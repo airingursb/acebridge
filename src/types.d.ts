506ba37a7d7620af07a47862702d448cfec5ff0e
Airing <airingdeng@tencent.com>
2024-07-07 14:08:09 +0800

    feat: 业务领域

--- src/types.d.ts
+++ src/types.d.ts
@@ -268,6 +268,7 @@ export interface Steps extends Omit<Headline, 'classes'>, Widget {
     description?: string;
     icon?: string;
     classes?: Record<string, string>;
+    imageSrc?: string;
   }>;
   callToAction?: string | CallToAction;
   image?: string | Image;
